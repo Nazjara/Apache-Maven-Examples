7649ed6f7a57ddd9a25d1658c79505541690e90c
Nazar Lipkovskyy <nazar.lipkovskyy@wds.co>
2019-05-20 13:52:12 +0300

    Add maven-failsafe-plugin

---
+++ src/test/java/com/nazjara/HelloWorldIT.java
@@ -0,0 +1,13 @@
+package com.nazjara;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldIT {
+
+    @Test
+    public void getHello() {
+        System.out.println("IT ran");
+    }
+}
